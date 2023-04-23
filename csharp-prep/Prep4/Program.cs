69421c5ee5935459e99dc9bf97451b74def13b06
P.T Nyamuda <ptnrlab@gmail.com>
2023-04-23 03:08:19 +0200

    prep4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> allNumbers = new List<int>();
+
+
+        int num = 1;
+        int sum = 0;
+        float average;
+        float largest;
+
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            allNumbers.Add(num);
+
+        }
+
+        foreach (int val in allNumbers)
+        {
+            sum += val;
+        }
+
+        average = (sum / allNumbers.Count);
+        largest = allNumbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+
+
     }
 }
